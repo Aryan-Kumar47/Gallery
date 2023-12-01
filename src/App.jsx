c871e8e5478228a544dedeef5172f34cc1d1acf8
Aryan-Kumar47 <kumarayan1012@gmail.com>
2023-12-01 11:29:28 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Com from "./components/Com";
 const App = () => {
 
   return (
-    <div className="w-screen h-screen relative overflow-hidden" style={{overflow: 'hidden'}}>
+    <div className="w-screen h-screen relative " style={{overflowX: 'hidden'}}>
       <Com/>
     </div>
   );
